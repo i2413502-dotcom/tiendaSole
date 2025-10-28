45f73ffe4e7dc9432dbfc10e3944feadad91ab0f
i2413502-dotcom <i2413502@continental.edu.pe>
2025-10-28 14:43:13 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
-// supabaseClient.js
+// db.js
 const { createClient } = require('@supabase/supabase-js')
 
-const supabaseUrl = 'https://xbktrcfwmzukqyffpjss.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const SUPABASE_URL = 'https://dkouucghnlhxgtgsxsfd.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable_Lta1ePe4rLripLCwq_u8Lw_YazBAAbM'
+
+// Cliente Supabase
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 module.exports = { supabase }
