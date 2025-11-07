e49cdc47219735bd6dbd0e5f12caa9c45e78fc31
i2413502-dotcom <i2413502@continental.edu.pe>
2025-11-07 07:22:25 -0500

    Change Supabase URL and anon key
    Updated Supabase URL and anon key in db.js.

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 // db.js
 const { createClient } = require('@supabase/supabase-js')
 
-const SUPABASE_URL = 'https://dkouucghnlhxgtgsxsfd.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_Lta1ePe4rLripLCwq_u8Lw_YazBAAbM'
+const SUPABASE_URL = 'https://xbktrcfwmzukqyffpjss.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhia3RyY2Z3bXp1a3F5ZmZwanNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2MTY3OTgsImV4cCI6MjA3NzE5Mjc5OH0.hKlD2X6bC_yBeawLJmnewBBQ9JjEz5J8WoqBPKpcHkw'
 
 // Cliente Supabase
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
